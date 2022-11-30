c25706824f9b6fbff4ac95bfbb26708600bd96a1
Victor Vilela Duarte <victorvduarte@gmail.com>
2022-11-30 00:42:12 -0300

    highlight component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,12 @@
 import { Header } from "@components/Header";
-import { Container, Title } from "./styles";
+import { HighLight } from "@components/Highlight";
+import { Container } from "./styles";
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <HighLight title="Turmas" subtitle="Jogue com a sua turma" />
     </Container>
   );
 }
