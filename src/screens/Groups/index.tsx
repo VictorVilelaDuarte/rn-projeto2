5eebf1762fd5f924e2dad7eba5ed67f2846f7f34
Victor Vilela Duarte <victorvduarte@gmail.com>
2022-11-30 01:55:15 -0300

    Button component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,18 +5,19 @@ import { Header } from "@components/Header";
 import { HighLight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 import { Container } from "./styles";
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([
-    // "Galera do Ba",
-    // "Lagoinha",
-    // "Time da Kobe",
-    // "Grupo da familia",
-    // "TPM",
-    // "TVM",
-    // "Academia",
+    "Galera do Ba",
+    "Lagoinha",
+    "Time da Kobe",
+    "Grupo da familia",
+    "TPM",
+    "TVM",
+    "Academia",
   ]);
 
   return (
@@ -31,6 +32,7 @@ export function Groups() {
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={<ListEmpty message="Nenhum grupo encontrado" />}
       />
+      <Button title="Criar nova turma" />
     </Container>
   );
 }
