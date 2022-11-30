ff2be80d338d75d0847d07df591178ddf65b386a
Victor Vilela Duarte <victorvduarte@gmail.com>
2022-11-29 23:52:33 -0300

    statusbar, loading and fonts

--- App.tsx
+++ App.tsx
@@ -1,13 +1,28 @@
+import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import { Loading } from "@components/Loading";
 
 import theme from "@theme/index";
 
 import { Groups } from "@screens/Groups";
 
 export default function App() {
+  const [fontsLoades] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoades ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
