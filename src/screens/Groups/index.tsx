2bafeb6177353b9dc60384c624a288f30ec93702
Victor Vilela Duarte <victorvduarte@gmail.com>
2022-11-30 01:18:43 -0300

    flatlist group

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+
 import { Header } from "@components/Header";
 import { HighLight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
@@ -5,12 +8,26 @@ import { GroupCard } from "@components/GroupCard";
 import { Container } from "./styles";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([
+    "Galera do Ba",
+    "Lagoinha",
+    "Time da Kobe",
+    "Grupo da familia",
+    "TPM",
+    "TVM",
+    "Academia",
+  ]);
+
   return (
     <Container>
       <Header />
       <HighLight title="Turmas" subtitle="Jogue com a sua turma" />
 
-      <GroupCard title="Galera do Ba" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 }
