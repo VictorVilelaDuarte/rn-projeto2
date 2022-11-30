84c10b83db9316082c9693a00a0daca0e23442f6
Victor Vilela Duarte <victorvduarte@gmail.com>
2022-11-30 00:58:27 -0300

    card component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,5 +1,7 @@
 import { Header } from "@components/Header";
 import { HighLight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
+
 import { Container } from "./styles";
 
 export function Groups() {
@@ -7,6 +9,8 @@ export function Groups() {
     <Container>
       <Header />
       <HighLight title="Turmas" subtitle="Jogue com a sua turma" />
+
+      <GroupCard title="Galera do Ba" />
     </Container>
   );
 }
